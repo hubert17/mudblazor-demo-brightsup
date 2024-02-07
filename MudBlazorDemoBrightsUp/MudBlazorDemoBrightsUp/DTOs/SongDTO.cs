0a81c9176ccf5709b5f8368f597b0460e3dd5133
hubert17 <hewbertgabon@gmail.com>
2024-02-08 06:21:54 +0800

    added Song Crud

--- MudBlazorDemoBrightsUp/MudBlazorDemoBrightsUp/DTOs/SongDTO.cs
+++ MudBlazorDemoBrightsUp/MudBlazorDemoBrightsUp/DTOs/SongDTO.cs
@@ -1,14 +1,14 @@
-﻿namespace MudBlazorDemoBrightsUp.DTOs
+﻿using MudBlazorDemoBrightsUp.Models;
+using static MudBlazor.CategoryTypes;
+
+namespace MudBlazorDemoBrightsUp.DTOs
 {
     public class SongDTO
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Artist { get; set; }
-        public string Duration { get; set; }
-        public int PeakChartPosition { get; set; }
-        public int ReleaseYear { get; set; }
-        public string RecordLabel { get; set; }
-        public string Genre { get; set; }
+        public int pageNumber { get; set; }
+        public int pageSize { get; set; }
+        public int totalItemCount { get; set; }
+        public int totalPageCount { get; set; }
+        public List<SongModel> items { get; set; }
     }
 }
---
+++ MudBlazorDemoBrightsUp/MudBlazorDemoBrightsUp/Models/SongModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MudBlazorDemoBrightsUp.Models
+{
+    public class SongModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Duration { get; set; }
+        public int PeakChartPosition { get; set; }
+        public int ReleaseYear { get; set; }
+        public string RecordLabel { get; set; }
+        public string Genre { get; set; }
+    }
+}
